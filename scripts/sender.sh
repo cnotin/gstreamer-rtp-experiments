a15b5bc8955f8624558fc4c2e3cd665d019eeb4c
ClementNotin <clement@notin.org>
2012-11-21 21:38:17 +0100

    Add feature to pass as first parameter the frequency of the generated sine by sender script

--- scripts/sender.sh
+++ scripts/sender.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
 
+if [ $1 ]
+then
+	FREQ=$1
+else
+	FREQ=440;
+fi;
+
 gst-launch --gst-debug=2 --gst-debug-no-color gstrtpbin name=rtpbin \
-    audiotestsrc ! audioconvert ! audio/x-raw-int,channels=1,depth=16,width=16,rate=44100 ! \
+    audiotestsrc freq=$FREQ ! audioconvert ! audio/x-raw-int,channels=1,depth=16,width=16,rate=44100 ! \
     rtpL16pay ! rtpbin.send_rtp_sink_1 \
           rtpbin.send_rtp_src_1 ! udpsink port=5002 \
           rtpbin.send_rtcp_src_1 ! udpsink port=5003 sync=false async=false
