502b474961ae76b495bf71f4310853a4be90cc87
ClementNotin <clement@notin.org>
2012-11-26 18:23:42 +0100

    The minial example work with the scripts but still no joy in java even with a lot of simplification (removal of adder)

--- scripts/sender.sh
+++ scripts/sender.sh
@@ -7,9 +7,9 @@ else
 	FREQ=440;
 fi;
 
-gst-launch --gst-debug=2 --gst-debug-no-color gstrtpbin name=rtpbin \
-    audiotestsrc freq=$FREQ ! audioconvert ! audio/x-raw-int,channels=1,depth=16,width=16,rate=44100 ! \
-    rtpL16pay ! rtpbin.send_rtp_sink_1 \
-          rtpbin.send_rtp_src_1 ! udpsink port=5002 \
-          rtpbin.send_rtcp_src_1 ! udpsink port=5003 sync=false async=false
+gst-launch -v --gst-debug=2 gstrtpbin name=rtpbin \
+    audiotestsrc freq=$FREQ ! audioconvert ! \
+    mulawenc ! rtppcmupay ! rtpbin.send_rtp_sink_0 \
+          rtpbin.send_rtp_src_0 ! udpsink port=5002
+#          rtpbin.send_rtcp_src_0 ! udpsink port=5003 sync=false async=false
 #    udpsrc port=5007 ! rtpbin.recv_rtcp_sink_0
--- src/main/java/Receiver.java
+++ src/main/java/Receiver.java
@@ -1,125 +1,81 @@
 import org.gstreamer.Caps;
-import org.gstreamer.Closure;
 import org.gstreamer.Element;
 import org.gstreamer.ElementFactory;
 import org.gstreamer.Gst;
 import org.gstreamer.Pad;
+import org.gstreamer.PadLinkReturn;
 import org.gstreamer.Pipeline;
 
 public class Receiver {
 	public static void main(String[] args) {
 		Gst.init("Receiver", new String[] { "--gst-debug-level=2",
-				"--gst-debug=adder:4", "--gst-debug-no-color" });
+				"--gst-debug=liveadder:4", "--gst-debug=basesrc:4",
+				"--gst-debug-no-color" });
 
+		// ################# CREATE AND CONFIGURE ELEMENTS ##################
 		final Pipeline pipeline = new Pipeline("pipeline");
 
-		Element rtpSource = ElementFactory.make("udpsrc", "rtpSource");
-		rtpSource.set("port", 5002);
-		System.out.println("caps "
-				+ rtpSource
+		final Element udpSource = ElementFactory.make("udpsrc", null);
+		udpSource.set("port", 5002);
+		successOrDie(
+				"caps",
+				udpSource
 						.getSrcPads()
 						.get(0)
 						.setCaps(
-								Caps.fromString("application/x-rtp, "
-										+ "media=(string)audio, "
-										+ "clock-rate=(int)44100, "
-										+ "encoding-name=(string)L16, "
-										+ "encoding-params=(string)1, "
-										+ "channels=(int)1, "
-										+ "payload=(int)96")));
-		Element rtcpSource = ElementFactory.make("udpsrc", "rtcpSource");
-		rtcpSource.set("port", 5003);
+								Caps.fromString("application/x-rtp, media=audio, "
+										+ "clock-rate=8000, channel=1, payload=0, "
+										+ "encoding-name=PCMU")));
 
-		final Element adder = ElementFactory.make("adder", "adder");
-		adder.set("caps", Caps.fromString("audio/x-raw, "
-				+ "format=(string)S16LE, rate=(int)44100, "
-				+ "channels=(int)1, layout=(string)interleaved"));
-		System.out.println("adder caps " + adder.get("caps"));
+		final Element rtpBin = ElementFactory.make("gstrtpbin", null);
+		final Element sinkQueue = ElementFactory.make("queue", null);
+		final Element sink = ElementFactory.make("autoaudiosink", null);
 
-		final Element rtpBin = ElementFactory.make("gstrtpbin", "rtpbin");
-		rtpBin.set("buffer-mode", 0);
 		rtpBin.connect(new Element.PAD_ADDED() {
 			@Override
 			public void padAdded(Element element, Pad pad) {
 				System.out.println("Pad added: " + pad);
 				if (pad.getName().startsWith("recv_rtp_src")) {
-					System.out.println("\nGot new sound input pad: " + pad);
+					Element fake = ElementFactory.make("fdsink", null);
+					pipeline.add(fake);
+					pad.link(fake.getStaticPad("sink"));
 
-					final Element rtpDepayload = ElementFactory.make(
-							"rtpL16depay", null);
-					final Element convert = ElementFactory.make("audioconvert",
-							null);
-					pipeline.add(rtpDepayload);
-					rtpDepayload.syncStateWithParent();
-					pipeline.add(convert);
-					convert.syncStateWithParent();
-
-					System.out.println("bin-payload "
-							+ Element.linkPads(rtpBin, pad.getName(),
-									rtpDepayload, null));
-
-					Pad adderPad = adder.getRequestPad("sink%d");
-
-					System.out.println("depayload-convert "
-							+ Element.linkMany(rtpDepayload, convert));
-					System.out.println(convert + "-" + adderPad + " "
-							+ convert.getStaticPad("src").link(adderPad)
-							+ convert.getSrcPads().get(0).getCaps() + " -- "
-							+ adderPad.getCaps());
+					// System.out.println("\nGot new sound input pad: " + pad);
+					//
+					// // create elements
+					// DecodeBin decoder = new DecodeBin();
+					//
+					// // add them
+					// pipeline.add(decoder);
+					//
+					// // sync them
+					// decoder.syncStateWithParent();
+					//
+					// // link them
+					// successOrDie(
+					// "bin-decoder",
+					// pad.link(decoder.getStaticPad("sink")).equals(
+					// PadLinkReturn.OK));
+					//
+					// successOrDie(
+					// "decoder-sinkqueue",
+					// decoder.getStaticPad("src")
+					// .link(sinkQueue.getStaticPad("sink"))
+					// .equals(PadLinkReturn.OK));
 				}
 			}
 		});
-		rtpBin.connect(new Element.PAD_REMOVED() {
-			@Override
-			public void padRemoved(Element element, Pad pad) {
-				System.out.println("bye bye");
-			}
-		});
-		rtpBin.connect("on-bye-timeout", new Closure() {
-			@SuppressWarnings("unused")
-			public void invoke() {
-				System.out.println("on-bye-timeout");
-			}
-		});
-		rtpBin.connect("on-timeout", new Closure() {
-			@SuppressWarnings("unused")
-			public void invoke() {
-				System.out.println("on-timeout");
-			}
-		});
-		rtpBin.connect("on-bye-ssrc", new Closure() {
-			@SuppressWarnings("unused")
-			public void invoke() {
-				System.out.println("on-bye-ssrc");
-			}
-		});
-		rtpBin.set("autoremove", true);
-		rtpBin.set("use-pipeline-clock", true);
-		rtpBin.set("ntp-sync", true);
-		rtpBin.getRequestPad("recv_rtp_sink_0");
-		rtpBin.getRequestPad("recv_rtcp_sink_0");
-		rtpBin.connect("on-new-ssrc", new Closure() {
-			@SuppressWarnings("unused")
-			public void invoke(Element element, int session, int ssrc) {
-				System.out.println("New SSRC (uint)" + ssrc + "/"
-						+ String.format("%X", ssrc));
-			}
-		});
 
-		Element sink = ElementFactory.make("autoaudiosink", "sink");
+		// ############## ADD THEM TO PIPELINE ####################
+		pipeline.addMany(udpSource, rtpBin, sinkQueue, sink);
 
-		pipeline.addMany(rtpSource, rtcpSource, rtpBin, adder, sink);
+		// ###################### LINK THEM ##########################
+		Pad pad = rtpBin.getRequestPad("recv_rtp_sink_0");
+		successOrDie("udpSource-rtpbin", udpSource.getStaticPad("src")
+				.link(pad).equals(PadLinkReturn.OK));
 
-		System.out.println("final elts of stream "
-				+ Element.linkMany(adder, sink));
-
-		System.out.println("rtp "
-				+ Element.linkPads(rtpSource, null, rtpBin, "recv_rtp_sink_0"));
-		System.out
-				.println("rtcp "
-						+ Element.linkPads(rtcpSource, null, rtpBin,
-								"recv_rtcp_sink_0"));
-
+		successOrDie("adder-queue-sink ", Element.linkMany(sinkQueue, sink));
+		// ################## ROCK'n'ROLL #############################
 		pipeline.play();
 
 		System.out.println("Reactivate ?");
@@ -128,5 +84,12 @@ public class Receiver {
 		System.out.println("Bye ?");
 		new java.util.Scanner(System.in).nextLine();
 		System.out.println("Bye");
+	}
+
+	private static void successOrDie(String message, boolean result) {
+		if (!result) {
+			System.err.println("Die because of " + message);
+			System.exit(-1);
+		}
 	}
 }
